c0a203de31be7db229ec879b67eb71c9dc131b41
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2023-12-12 13:40:16 -0600

    init: create index

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import youtubeRoutes from './routes/youtube.js';
+import facebookRoutes from './routes/facebook.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.use('/youtube', youtubeRoutes);
+app.use('/facebook', facebookRoutes);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`\n\nServidor escuchando en http://localhost:${PORT}\n\n`);
+});
